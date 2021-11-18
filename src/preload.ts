c2454ce0803eef6996b1c89a683e3661b5fa4938
Gabriel Pupim <gabriel.pda15@gmail.com>
2021-11-18 20:18:17 -0300

    Changed the order of script preload

--- src/preload.ts
+++ src/preload.ts
@@ -12,13 +12,13 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     );
 
+    const exportsDeclaration = document.createElement('script');
+    exportsDeclaration.type = 'text/javascript';
+    exportsDeclaration.innerHTML = 'const { setTimeout, setInterval } = require(\'timers\'); var exports = {};';
+    document.head.appendChild(exportsDeclaration);
+
     const renderer = document.createElement('script');
     renderer.type = 'text/javascript';
     renderer.src = 'renderer.js';
     document.body.appendChild(renderer);
-
-    const exportsDeclaration = document.createElement('script');
-    renderer.type = 'text/javascript';
-    exportsDeclaration.innerHTML = 'var exports = {};';
-    document.head.appendChild(exportsDeclaration);
 });
