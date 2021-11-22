d525a572dc7460de30f7ebc10d16ec827fcc4902
Gabriel Pupim <gabriel.pda15@gmail.com>
2021-11-22 11:28:41 -0300

    Renamed decorators

--- src/library/decorators.ts
+++ src/library/decorators.ts
@@ -1,15 +1,23 @@
 import { ComponentOptions } from './models/component-options';
 import { ModuleOptions } from './models/module-options';
 
-export function ElectronComponent(options: ComponentOptions, fileName: string) {
+export function Component(options: ComponentOptions, fileName: string) {
     return function (target: any): void {
         Reflect.defineMetadata('component-options', options, target);        
         Reflect.defineMetadata('component-file', fileName, target);
     };
 }
 
-export function ElectronModule(options: ModuleOptions) {
+export function Module(options: ModuleOptions) {
     return function (target: any): void {
         Reflect.defineMetadata('module-options', options, target);
     }
+}
+
+export function ViewChild(id: string) {
+    return function (target: any, propertyKey: string): void {
+        Object.defineProperty(target, propertyKey, {
+            get: () => document.getElementById(id)
+        });
+    };
 }
