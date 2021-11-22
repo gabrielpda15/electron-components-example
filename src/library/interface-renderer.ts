58f65cebbf7b3e668239da12d20bc2b883617fa9
Gabriel Pupim <gabriel.pda15@gmail.com>
2021-11-22 10:46:06 -0300

    Extern interface renderer

---
+++ src/library/interface-renderer.ts
@@ -0,0 +1,84 @@
+import fs from 'fs';
+import path from 'path';
+import { AppModule } from '../app/app.module';
+import { ComponentOptions } from './models/component-options';
+import { ModuleOptions } from './models/module-options';
+
+export class InterfaceRenderer {
+
+    public rootNode: Element = null;
+    public modulesOutlets: HTMLCollectionOf<Element> = null;
+
+    constructor() { }
+
+    public render(): void {
+        this.loadModule(AppModule);
+        this.modulesOutlets = document.getElementsByTagName('module-outlet');
+        this.loadSubModules(AppModule);
+    }
+
+    private loadModule(module: any, parent?: Element): void {
+        const options: ModuleOptions = Reflect.getMetadata('module-options', module);
+    
+        if (parent && options?.bootstrap) {
+            this.loadComponent(options.bootstrap, parent);
+        } else if (options?.bootstrap) {
+            const bootstrapOptions: ComponentOptions = Reflect.getMetadata('component-options', options.bootstrap);
+            const tag = document.createElement(bootstrapOptions.selector);
+            if (this.rootNode) {
+                this.rootNode.append(tag);
+            } else {
+                this.rootNode = tag;
+                document.body.prepend(tag);
+            }
+            this.loadComponent(options.bootstrap);
+        }
+    
+        if (options?.declarations && options.declarations.length > 0) {
+            options.declarations.forEach(d => {
+                this.loadComponent(d);
+            });
+        }
+    }
+
+    private loadSubModules(module: any): void {
+        const options: ModuleOptions = Reflect.getMetadata('module-options', module);
+    
+        if (options?.imports && options.imports.length > 0) {
+            options.imports.forEach(m => {
+                this.loadModule(m, this.findModuleOutlet(m.name));
+                this.loadSubModules(m);
+            });
+        }
+    }
+
+    private loadComponent(component: any, parent?: Element): void {
+        const options: ComponentOptions = Reflect.getMetadata('component-options', component);
+        if (options) {
+            if (parent) {
+                parent.outerHTML = `<${options.selector}></${options.selector}>`;
+            }
+    
+            const elements = document.getElementsByTagName(options.selector);
+            let template = options.template
+    
+            if (!template) {
+                const file = Reflect.getMetadata('component-file', component);
+                template = fs.readFileSync(path.resolve(path.dirname(file), options.templateUrl), { encoding: 'utf-8' });
+            }
+    
+            for (let i = 0; i < elements.length; i++) {
+                elements.item(i).innerHTML = template;
+            }
+        }
+    }
+
+    private findModuleOutlet(name: string): Element {
+        for (let i = 0; i < this.modulesOutlets.length; i++) {
+            if (this.modulesOutlets.item(i).attributes.getNamedItem('name').value === name) {
+                return this.modulesOutlets.item(i);
+            }
+        }
+    }
+
+}
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,78 +1,8 @@
 import 'reflect-metadata';
-import glob from 'glob';
-import path from 'path';
-import fs from 'fs';
+import { InterfaceRenderer } from './library/interface-renderer';
 
-import { AppModule } from './app/app.module';
-import { ModuleOptions } from './library/models/module-options';
-import { ComponentOptions } from './library/models/component-options';
+const renderer = new InterfaceRenderer();
 
-let rootNode: Element = null;
-
-loadModule(AppModule);
-const modulesOutlets = document.getElementsByTagName('module-outlet');
-loadSubModules(AppModule);
-
-function loadModule(module: any, parent?: Element) {
-    const options: ModuleOptions = Reflect.getMetadata('module-options', module);
-
-    if (parent && options?.bootstrap) {
-        loadComponent(options.bootstrap, parent);
-    } else if (options?.bootstrap) {
-        const bootstrapOptions: ComponentOptions = Reflect.getMetadata('component-options', options.bootstrap);
-        const tag = document.createElement(bootstrapOptions.selector);
-        if (rootNode) {
-            rootNode.append(tag);
-        } else {
-            rootNode = tag;
-            document.body.prepend(tag);
-        }
-        loadComponent(options.bootstrap);
-    }
-
-    if (options?.declarations && options.declarations.length > 0) {
-        options.declarations.forEach(d => {
-            loadComponent(d);
-        });
-    }
-}
-
-function loadSubModules(module: any) {
-    const options: ModuleOptions = Reflect.getMetadata('module-options', module);
-
-    if (options?.imports && options.imports.length > 0) {
-        options.imports.forEach(m => {
-            loadModule(m, findModuleOutlet(m.name));
-            loadSubModules(m);
-        });
-    }
-}
-
-function loadComponent(component: any, parent?: Element) {
-    const options: ComponentOptions = Reflect.getMetadata('component-options', component);
-    if (options) {
-        if (parent) {
-            parent.outerHTML = `<${options.selector}></${options.selector}>`;
-        }
-
-        const elements = document.getElementsByTagName(options.selector);
-        let template = options.template
-
-        if (!template) {
-            const file = Reflect.getMetadata('component-file', component);
-            template = fs.readFileSync(path.resolve(path.dirname(file), options.templateUrl), { encoding: 'utf-8' });
-        }
-
-        for (let i = 0; i < elements.length; i++) {
-            elements.item(i).innerHTML = template;
-        }
-    }
-}
-
-function findModuleOutlet(name: string): Element {
-    for (let i = 0; i < modulesOutlets.length; i++) {
-        if (modulesOutlets.item(i).attributes.getNamedItem('name').value === name) {
-            return modulesOutlets.item(i);
-        }
-    }
-}
+(async () => {
+    renderer.render();
+})();
